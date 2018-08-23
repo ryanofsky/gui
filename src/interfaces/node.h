551c50873da0b0581a68c0b594ff0d87e6292947
Russell Yanofsky <russ@yanofsky.org>
2018-08-23 13:42:31 -0400

    gui: Partially revert #10244 gArgs and Params changes
    Change gui code to use gArgs, Params() functions directly instead of going
    through interfaces::Node.
    
    Remotely accessing bitcoin-node ArgsManager from bitcoin-gui works fine in
    https://github.com/bitcoin/bitcoin/pull/10102, when bitcoin-gui spawns a new
    bitcoin-node process and controls its startup, but for bitcoin-gui to support
    -ipcconnect option in https://github.com/bitcoin/bitcoin/pull/19461 and connect
    to an existing bitcoin-node process, it needs ability to parse arguments itself
    before connecting out.
    
    This change also simplifies https://github.com/bitcoin/bitcoin/pull/10102 a
    bit, by making the bitcoin-gui -> bitcoin-node startup sequence more similar to
    the bitcoin-node -> bitcoin-wallet startup sequence where the parent process
    parses arguments and passes them to the child process instead of the parent
    process using the child process to parse arguments.

--- src/interfaces/node.h
+++ src/interfaces/node.h
@@ -48,24 +48,6 @@ public:
     //! Send init error.
     virtual void initError(const bilingual_str& message) = 0;
 
-    //! Set command line arguments.
-    virtual bool parseParameters(int argc, const char* const argv[], std::string& error) = 0;
-
-    //! Set a command line argument
-    virtual void forceSetArg(const std::string& arg, const std::string& value) = 0;
-
-    //! Set a command line argument if it doesn't already have a value
-    virtual bool softSetArg(const std::string& arg, const std::string& value) = 0;
-
-    //! Set a command line boolean argument if it doesn't already have a value
-    virtual bool softSetBoolArg(const std::string& arg, bool value) = 0;
-
-    //! Load settings from configuration file.
-    virtual bool readConfigFiles(std::string& error) = 0;
-
-    //! Choose network parameters.
-    virtual void selectParams(const std::string& network) = 0;
-
     //! Get the (assumed) blockchain size.
     virtual uint64_t getAssumedBlockchainSize() = 0;
 
@@ -101,9 +83,6 @@ public:
 
     //! Return whether shutdown was requested.
     virtual bool shutdownRequested() = 0;
-
-    //! Setup arguments
-    virtual void setupServerArgs() = 0;
 
     //! Map port.
     virtual void mapPort(bool use_upnp) = 0;
